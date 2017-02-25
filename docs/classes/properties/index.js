66fe10cfa9bb0c0d43154c5a995d5e9bc4e8148c
John Figueiredo <johfi@extendhealth.com>
2017-02-25 13:42:52 -0700

    Adding changes for the constructors

---
+++ docs/classes/properties/index.js
@@ -0,0 +1,35 @@
+class Teacher {
+    constructor(name, students) {
+        this._name = name;
+        this._students = students;
+    }
+
+    addStudent(student) {
+        this._student
+    }
+}
+
+class Student {
+    constructor(name, tests) {
+        this._name = name;
+        this.test = tests;
+    }
+
+    addTest(test) {
+        this._tests
+    }
+
+    getAverage() {
+        this._tests.forEach(function(item, index, array) {
+            total += item.grade;
+        })
+    }
+};
+
+class Test {
+    constructor(name, grade) {
+        this._name = name;
+        this._grade = grade;
+    }
+};
+
